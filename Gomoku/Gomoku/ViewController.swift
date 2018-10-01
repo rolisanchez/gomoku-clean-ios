a964b16500dc9080624c0ef732aab43dfb19716a
Victor Rolando Sanchez Jara <roli.s91@gmail.com>
2018-10-02 00:11:28 +0800

    First commit

---
+++ Gomoku/Gomoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gomoku
+//
+//  Created by Victor Rolando Sanchez Jara on 2/10/18.
+//  Copyright © 2018 vrsj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
